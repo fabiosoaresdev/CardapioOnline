c44ef726ede529636497aac08468d21f60409843
Fábio Soares <fabiosooaresdev@gmail.com>
2023-10-10 17:20:18 -0300

    Botões do carrinho
    Adicionadas funcionalidades para os botões dos carrinhos, e algumas outras correções no site também foram realizadas.

--- js/app.js
+++ js/app.js
@@ -136,6 +136,7 @@ cardapio.metodos = {
     abrirCarrinho: (abrir) => {
         if (abrir) {
             $("#modalCarrinho").removeClass('hidden');
+            cardapio.metodos.carregarEtapa(1)
             }
         
 
@@ -144,9 +145,64 @@ cardapio.metodos = {
         }
     },
 
+    // Altera os textos e exibe os botões de etapa
+    carregarEtapa: (etapa) => {
+
+        if (etapa == 1) {
+            $("#lblTituloEtapa").text('Seu Carrinho:')
+            $("#itensCarrinho").removeClass('hidden')
+            $("#localEntrega").addClass('hidden')
+            $("#resumoCarrinho").addClass('hidden')
+
+            $(".etapa").removeClass('active')
+            $(".etapa1").addClass('active')
+
+            $("#btnEtapaPedido").removeClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").addClass('hidden')
+
+        }
+
+        if (etapa == 2) {
+            $("#lblTituloEtapa").text('Endereço:')
+            $("#itensCarrinho").addClass('hidden')
+            $("#localEntrega").removeClass('hidden')
+            $("#resumoCarrinho").addClass('hidden')
+
+            $(".etapa").removeClass('active')
+            $(".etapa1").addClass('active')
+            $(".etapa2").addClass('active')
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").removeClass('hidden')
+            $("#btnEtapaResumo").addClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
+        }
+
+        if (etapa == 3) {
+            $("#lblTituloEtapa").text('Resumo do pedido:')
+            $("#itensCarrinho").addClass('hidden')
+            $("#localEntrega").addClass('hidden')
+            $("#resumoCarrinho").removeClass('hidden')
+
+            $(".etapa").removeClass('active')
+            $(".etapa1").addClass('active')
+            $(".etapa2").addClass('active')
+            $(".etapa3").addClass('active')
+
+            $("#btnEtapaPedido").addClass('hidden')
+            $("#btnEtapaEndereco").addClass('hidden')
+            $("#btnEtapaResumo").removeClass('hidden')
+            $("#btnVoltar").removeClass('hidden')
+        }
+    },    
 
 
-
+    voltarEtapa: () => {
+        let etapa = $(".etapa.active").length
+        cardapio.metodos.carregarEtapa(etapa -1)
+    },
 
 
 
