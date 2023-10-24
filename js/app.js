d6a0ba83c7aeaf138d65a39ca1b5e432e92419cf
Fábio Soares <fabiosooaresdev@gmail.com>
2023-10-24 20:40:28 -0300

    API DE CEP
    Adicionada API de CEP no site, e algumas correções de bugs foram feitas.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    cardapio.eventos.init();
+    cardapio.eventos.init()
 })
 
-var cardapio = {};
+var cardapio = {}
 
-var MEU_CARRINHO = [];
-
+var MEU_CARRINHO = []
+var MEU_ENDERECO = null
 var VALOR_CARRINHO = 0
 var VALOR_ENTREGA = 7
 cardapio.eventos = {
@@ -18,7 +18,7 @@ cardapio.metodos = {
     //Obtem a lista de Itens do cardapio
     obterItensCardapio: (categoria = 'burgers', vermais = false) => {
         
-        let filtro = MENU[categoria];
+        let filtro = MENU[categoria]
         console.log(filtro)
         
         if(!vermais) {
@@ -33,7 +33,7 @@ cardapio.metodos = {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${name}/g, e.name)
             .replace(/\${price}/g, e.price.toFixed(2).replace('.' , ','))
-            .replace(/\${id}/g, e.id);
+            .replace(/\${id}/g, e.id)
 
             //Botao ver mais
             if(vermais && i >= 8 && i <12) {
@@ -47,16 +47,16 @@ cardapio.metodos = {
         })
 
         // remove o ativo
-        $(".container-menu a").removeClass('active');
+        $(".container-menu a").removeClass('active')
         // seta o menu para ativo
         $("#menu-" + categoria).addClass('active')
     },
 
     // clique no botão ver mais
     verMais: () => {
-        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1];
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]
         cardapio.metodos.obterItensCardapio(ativo, true)
-        $("#btnVerMais").addClass('hidden');
+        $("#btnVerMais").addClass('hidden')
     },
 
     // diminuir a quantidade do ítem no cardápio.
@@ -292,6 +292,133 @@ cardapio.metodos = {
         })
     },
 
+    //Carrega a etapa de endereço
+    carregarEndereco: () => {
+        if (MEU_CARRINHO.length <= 0) {
+            cardapio.metodos.mensagem('Seu carrinho está vazio.')
+            return;
+        }
+
+        cardapio.metodos.carregarEtapa(2)
+
+    },
+
+    //API DE CEP
+    buscarCEP: () => {
+        var cep  = $("#txtCEP").val().trim().replace(/\/D\g/, '')
+
+        //Vrerifica se o usuário informou um CEP na caixa de texto
+        if (cep != "") {
+            //Expressão regular para validar CEP
+            var validacep =  /^[0-9]{8}$/
+
+            if(validacep.test(cep)) {
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                if(!("erro" in dados)) {
+                    //Atualiza os campos com os valores retornados
+                    $("#txtEndereco").val(dados.logradouro)
+                    $("#txtBairro").val(dados.bairro)
+                    $("#txtCidade").val(dados.localidade)
+                    $("#ddlUf").val(dados.uf)
+                    $("#txtNumero").focus()
+                }
+
+                else {
+                    cardapio.metodos.mensagem('CEP Não encontrado. Preencha as informações manualmente.')
+                    $("#txtEndereco").focus()
+                }
+                })
+            }
+
+            else {
+                cardapio.metodos.mensagem('Formato de CEP inválido, tente novamente')
+                $("#txtCEP").focus()
+            }
+        }
+
+        else {
+            cardapio.metodos.mensagem('Por gentileza, informe o CEP na caixa selecionada:')
+            $("#txtCEP").focus()
+        }
+    },
+
+    //Validação antes de prosseguir para a etapa 3
+    resumoPedido: () => {
+        let cep = $("#txtCEP").val().trim()
+        let endereco = $("#txtEndereco").val().trim()
+        let bairro = $("#txtBairro").val().trim()
+        let cidade = $("#txtCidade").val().trim()
+        let uf = $("#ddlUf").val().trim()
+        let numero = $("#txtNumero").val().trim()
+        let complemento = $("#txtComplemento").val().trim()
+
+        if (cep.length <= 0) {
+            cardapio.metodos.mensagem("Por gentileza, informe o CEP:")
+            $("#txtCEP").focus()
+            return;
+        }
+
+        if (endereco.length <= 0) {
+            cardapio.metodos.mensagem("Por gentileza, informe o Endereço:")
+            $("#txtEndereco").focus()
+            return;
+        }
+
+        if (bairro.length <= 0) {
+            cardapio.metodos.mensagem("Por gentileza, informe o Bairro:")
+            $("#txtBairro").focus()
+            return;
+        }
+
+        if (cidade.length <= 0) {
+            cardapio.metodos.mensagem("Por gentileza, informe a Cidade:")
+            $("#txtCidade").focus()
+            return;
+        }
+
+        if (uf == -1) {
+            cardapio.metodos.mensagem("Por gentileza, informe a uf:")
+            $("#ddlUf").focus()
+            return;
+        }
+
+        if (numero.length <= 0) {
+            cardapio.metodos.mensagem("Por gentileza, informe o Número da residência:")
+            $("#txtNumero").focus()
+            return;
+        }
+
+        MEU_ENDERECO = {
+            cep: cep,
+            endereco: endereco,
+            bairro: bairro,
+            cidade: cidade,
+            uf: uf,
+            numero: numero,
+            complemento: complemento
+        }
+        cardapio.metodos.carregarEtapa(3)
+        cardapio.metodos.carregarResumo()
+    },
+
+    //Carrega a etapa de resumo do carrinho
+    carregarResumo: () => {
+        $("#listaItensResumo").html('')
+
+        $.each(MEU_CARRINHO, (i,e) =>  {
+            let temp = cardapio.templates.itemResumo.replace(/\${img}/g, e.img)
+                .replace(/\${name}/g, e.name)
+                .replace(/\${price}/g, e.price.toFixed(2).replace('.' , ','))
+                .replace(/\${qntd}/g, e.qntd);
+
+                $("#listaItensResumo").append(temp)
+        })
+
+        $("#resumoEndereco").html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`)
+        $("#cidadeEndereco").html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`)
+    },
+
     mensagem: (texto, cor = 'red', tempo = 3500) => {
 
         let id = Math.floor(Date.now() * Math.random()).toString();
@@ -349,5 +476,23 @@ cardapio.templates = {
                 <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fas fa-plus"></i></span>
                 <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
             </div>
+        </div>`,
+
+    itemResumo: `
+        <div class="col-12 item-carrinho resumo">
+            <div class="img-carrinho-resumo">
+                <img src="\${img}" alt="">
+            </div>
+            <div class="dados-produto">
+                <p class="title-produto-resumo">
+                    <strong>\${name}</strong>
+                </p>
+                <p class="price-produto-resumo">
+                    <strong>\${price}</strong>
+                </p>
+            </div>
+            <p class="quantidade-produto-resumo">
+                x <strong>\${qntd}</strong>
+            </p>
         </div>`
 }
